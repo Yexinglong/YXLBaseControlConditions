f2949fdf2241452a11cfd85f0b4ec5d9e7cfec37
ZHY <ZHY@192.168.0.101>
2016-10-24 11:16:49 +0800

    no message

---
+++ UIImageView+YXLAcronymImageView.m
@@ -0,0 +1,35 @@
+//
+//  UIImageView+YXLAcronymImageView.m
+//  田田
+//
+//  Created by ZHY on 2016/9/12.
+//  Copyright © 2016年 叶星龙. All rights reserved.
+//
+
+#import "UIImageView+YXLAcronymImageView.h"
+
+@implementation UIImageView (YXLAcronymImageView)
+
+- (UIImageView * (^)(id image))kImage{
+    return ^id(id image){
+            if ([image isKindOfClass:[NSString class]]) {
+                [self setImage:[UIImage imageNamed:image]];
+            }else if ([image isKindOfClass:[UIImage class]]){
+                [self setImage:image];
+            }
+        return self;
+    };
+}
+
+- (UIImageView * (^)(id image))kHighlightedImage{
+    return ^id(id image){
+            if ([image isKindOfClass:[NSString class]]) {
+                [self setHighlightedImage:[UIImage imageNamed:image]];
+            }else if ([image isKindOfClass:[UIImage class]]){
+                [self setHighlightedImage:image];
+            }
+        return self;
+    };
+}
+
+@end
